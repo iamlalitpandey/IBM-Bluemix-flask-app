242a834415997b5c999baba677968fe615dc1f77
iamlalitpandey <lalitatg@gmail.com>
2016-06-30 16:31:09 -0400

    updating

--- welcome.py
+++ welcome.py
@@ -27,12 +27,12 @@
 	# #return app.send_static_file('index.html')
 
 	
-# @app.route('/myapp')
-# def WelcomeToMyapp():
-    # return 'Welcome again to my app running on Bluemix!oooooooooooooooooooooonew'
+ @app.route('/myapp')
+ def WelcomeToMyapp():
+     return 'Welcome again to my app running on Bluemix!oooooooooooooooooooooonew'
 
- #port = os.getenv('PORT', '8080')
- port = int(os.getenv('VCAP_APP_PORT', '8080'))
+ port = os.getenv('PORT', '8080')
+ #port = int(os.getenv('VCAP_APP_PORT', '5000'))
 
  if __name__ == "__main__":
 	 app.run(host='0.0.0.0',port=port)
