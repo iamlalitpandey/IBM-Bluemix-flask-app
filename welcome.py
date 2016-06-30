e4a33be3dfe05ccf27443e536647b23786d19756
iamlalitpandey <lalitatg@gmail.com>
2016-06-30 16:22:39 -0400

    updating

--- welcome.py
+++ welcome.py
@@ -31,8 +31,8 @@
 # def WelcomeToMyapp():
     # return 'Welcome again to my app running on Bluemix!oooooooooooooooooooooonew'
 
-# #port = os.getenv('PORT', '8080')
- port = int(os.getenv('VCAP_APP_PORT', '5000'))
+ port = os.getenv('PORT', '8080')
+ #port = int(os.getenv('VCAP_APP_PORT', '5000'))
 
  if __name__ == "__main__":
 	 app.run(host='0.0.0.0',port=port)
