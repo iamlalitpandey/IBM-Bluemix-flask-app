06b5de7bf8827e4c3d2bd84f550f99cb84f75e5b
iamlalitpandey <lalitatg@gmail.com>
2016-06-30 16:20:15 -0400

    updating

--- welcome.py
+++ welcome.py
@@ -32,10 +32,10 @@
     # return 'Welcome again to my app running on Bluemix!oooooooooooooooooooooonew'
 
 # #port = os.getenv('PORT', '8080')
-# port = int(os.getenv('VCAP_APP_PORT', '5000'))
+ port = int(os.getenv('VCAP_APP_PORT', '5000'))
 
-# if __name__ == "__main__":
-	# app.run(host='0.0.0.0',port=port)
+ if __name__ == "__main__":
+	 app.run(host='0.0.0.0',port=port)
 	
 	# import json
 	# from watson_developer_cloud import RetrieveAndRankV1
